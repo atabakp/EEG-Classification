083fc8e993f500d014c3bbfcb38b277da79489f4
Cisco <cisco@Ciscos-MacBook-Air.local>
2018-07-18 00:56:24 -0700

    added print msg

--- feature_extracting.py
+++ feature_extracting.py
@@ -110,34 +110,50 @@ def multitaper_2D(EEG, npts=20, fw=3, number_of_tapers=5, fs=100,
 def main():
     EEG = np.load('EEG.npy')
     # Wavelet Spectogram concatinate
+    print('Wavelet 1D width=50...')
     wave_spec(EEG, width=5, wavelet=signal.morlet, filename='cwt-1D-5')
 
+    print('Wavelet 2D width=50...')
     # Wavelet Spectogram 2D
     wave_spec_2D(EEG, width=5, wavelet=signal.morlet, filename='cwt-1D-5')
 
     # Short-Time Fourier Transform
+    print('SFTF 1D fs=50...')
     short_time_ft(EEG, fs=50, filename='stft-1D-50')
+    print('SFTF 1D fs=100...')
     short_time_ft(EEG, fs=100, filename='stft-1D-100')
+    print('SFTF 1D fs=150...')
     short_time_ft(EEG, fs=150, filename='stft-1D-150')
 
     # Short-Time Fourier Transform 2D
+    print('SFTF 2D fs=50...')
     short_time_ft_2D(EEG, fs=50, filename='stft-2D-50')
+    print('SFTF 2D fs=100...')
     short_time_ft_2D(EEG, fs=100, filename='stft-2D-100')
+    print('SFTF 2D fs=150...')
     short_time_ft_2D(EEG, fs=150, filename='stft-2D-150')
 
     # Multitaper spectogram
+    print('multitaper 1D fs=50...')
     multitaper(EEG, npts=20, fw=3, number_of_tapers=5, fs=50,
                filename='mt-1D-50')
+
+    print('multitaper 1D fs=100...')
     multitaper(EEG, npts=20, fw=3, number_of_tapers=5, fs=100,
                filename='mt-1D-100')
+
+    print('multitaper 1D fs=150...')
     multitaper(EEG, npts=20, fw=3, number_of_tapers=5, fs=150,
                filename='mt-1D-150')
 
     # Multitaper spectogram 2D
+    print('multitaper 2D fs=50...')
     multitaper_2D(EEG, npts=20, fw=3, number_of_tapers=5, fs=50,
                   filename='mt-2D-50')
+    print('multitaper 2D fs=100...')             
     multitaper_2D(EEG, npts=20, fw=3, number_of_tapers=5, fs=100,
                   filename='mt-2D-100')
+    print('multitaper 2D fs=150...')             
     multitaper_2D(EEG, npts=20, fw=3, number_of_tapers=5, fs=150,
                   filename='mt-2D-150')
 
